c9aaad4997b42f1dedd43c1185f02c186071a807
Sam <Samy@SamdeMacBook-Pro.local>
2015-06-29 06:50:33 +0800

    init project

---
+++ start.sh
@@ -0,0 +1,6 @@
+export LC_APP_ID=js94ftkf38gk4vikwjtlhnyb8yl15lls6esj34icbao1slhn
+export LC_APP_KEY=wnmen47qodnkq4ejasze6t62llzh32o6064zonynpyfjksmr
+export LC_APP_MASTER_KEY=rxz940ygdxbipiqal7qccf5va2pxbfjba8ya8wzrq77j6d1s
+export LC_APP_PORT=3000
+
+supervisor --no-restart-on exit server.js
